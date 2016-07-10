8e2a0d47e4c34f8556995cc75de9d794bca76c62
Ben Goodstein <fuzzylogiq@users.noreply.github.com>
2016-07-10 19:39:40 +0100

    Update recipe_tester.sh

--- recipe_tester.sh
+++ recipe_tester.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git clone https://github.com/fuzzylogiq/recipe_tester.git
-cd recipe_checker && ./recipe_tester.py ../*/*.munki.recipe
+git clone https://github.com/fuzzylogiq/orchard-recipe-testing.git
+./orchard-recipe-testing/recipe_tester.py ../*/*.munki.recipe
